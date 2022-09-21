7f8d02805d42f98ea90a0a30d6b6848c48e93270
Boaz <boaserem022@gmail.com>
2022-09-21 09:28:23 +0300

    randomized

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
+import React from "react";
+import {data} from "./data";
 
-function App() {
 
+export const  App = () => {
 
-  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-  let noMembers;
-  const chunkSize = data.length/noMembers;
+  const arrIndex = [];
+  // const noMembers= 0;
+  // const groupSize = data.length/noMembers;
   
-  const Randomize = ()=>{
-    const outputArray = [];
-    const randomInt = Math.floor(Math.random() * data.length);
-
-    let i;
-    let a;
-    for(a=0; a<noMembers; a++){
-      console.log(chunkSize);
-      for(i = 0; i < chunkSize; i++){
-        outputArray.push(randomInt);
-      }
+  const arrIndexGenetator = ()=>{
+    for(var i =0; i < data.length; i++) {
+      arrIndex.push(i);
     }
-   
+  //  
   }
+
+  const Randomize = () => {
+    arrIndexGenetator();
+
+    data.map((datum, index, data)=>{
+
+      for(var a =0; a < data.length; a++) {
+        if(arrIndex.indexOf(datum.id) !== -1) {
+      
+          datum.id = Math.floor(Math.random() * data.length);
+          arrIndex.pop(datum.id);
+        }
+      }
+      return data;
+
+    })
+  };
+
+  console.log(data);
 
   return (
     <div >
     
         <h1>Please input your required number per group:</h1>
 
-        <label value={noMembers} >Number per group</label>
+        <label  >Number per group</label>
 
         <input type="number" name="numberPerGroup"  required/>
 
@@ -41,4 +54,4 @@ function App() {
   );
 }
 
-export default App;
+
